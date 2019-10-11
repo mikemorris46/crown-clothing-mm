c9fc5c7326411f0dab9e321a07e4be59c4a586ab
mikemorris46 <mike.morris.wa@gmail.com>
2019-10-11 13:48:49 +0800

    Project created with create-react-app and added files for homepage

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { render } from 'react-dom'
+import './index.scss'
+import App from './App'
+
+const RootElement = document.getElementById('root')
+
+render(<App />, RootElement)
