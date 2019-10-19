25f3736cdbd4770c26f0c9b3ac70080ce197c971
mikemorris46 <mike.morris.wa@gmail.com>
2019-10-19 16:29:35 +0800

    Added redux and iplemented userReducer and action(s) to app and header components

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import store from 'redux/store'
 
 import './index.scss'
 import App from './App'
@@ -8,8 +10,10 @@ import App from './App'
 const RootElement = document.getElementById('root')
 
 render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   RootElement
 )
